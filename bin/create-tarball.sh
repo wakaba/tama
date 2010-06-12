02339ae571cf9f15499bf4f806450098b28881f4
Wakaba <w@suika.fam.cx>
2010-06-12 21:29:47 +0900

    tarball creation script

---
+++ bin/create-tarball.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+basename=tama-`ruby -e 'load "lib/tama_m.rb"; puts "#{TAMA::Version}"'`
+tarname=$basename.tar
+gzname=$tarname.gz
+if [ -f $gzname ]; then
+  echo "$gzname: File exists"
+  exit 1
+fi
+
+echo $tarname...
+
+mkdir $basename || exit 1
+
+cp -R --preserve=timestamps bin lib doc conf html tools config *.rb *.ja $basename
+tar -cf $tarname --exclude=*~ --atime-preserve --preserve-permission --owner apache --group apache $basename
+gzip $tarname
+
+rm -fr $basename
+
+## Author: Wakaba <w@suika.fam.cx>
+## License: Public Domain.
