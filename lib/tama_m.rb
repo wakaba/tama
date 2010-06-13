2365693cd166ee6bec68b934e5d62860931c4a5c
Wakaba <w@suika.fam.cx>
2010-06-13 17:49:07 +0900

    date

--- lib/tama_m.rb
+++ lib/tama_m.rb
@@ -4,6 +4,6 @@
 module TAMA
   Official = "http://www.wakaba.jp/~ikemo/soft/tama/"
   Version = "1.1.66-mod1"
-  Build = "2010-06-12"
+  Build = "2010-06-13"
   Agent = "TAMATEBAKO/#{Version} (#{Build})"
 end
